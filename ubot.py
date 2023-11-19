53819e57622b29479be97ac04dde3385916539fc
Supratim Saha <74930882+Edward876@users.noreply.github.com>
2023-11-19 15:34:03 +0530

    Update ubot.py

--- ubot.py
+++ ubot.py
@@ -82,7 +82,7 @@ async def view_coupons():
                 <style>
                     body {
                         font-family: 'Arial', sans-serif;
-                        background-image: url('./ac/image.jpg);
+                        background-image: url('ac/image.jpg);
                         background-size: cover
                         margin: 0;
                         padding: 20px;
