c9ff8ec8d9f7b7bbeddea53abad282a39ee9e95c
Supratim Saha <74930882+Edward876@users.noreply.github.com>
2023-11-20 21:37:24 +0530

    Update ubot.py

--- ubot.py
+++ ubot.py
@@ -78,7 +78,7 @@ async def view_coupons():
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Course Coupons</title>
+                <title>FreeEduFetch</title>
                 <style>
                     body {
                         font-family: 'Arial', sans-serif;
